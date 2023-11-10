012063641320a0fa201cbb869306904ed7179859
kess4obehi <kessiena4excellence@gmail.com>
2023-11-10 01:46:49 -0800

    smple shell

--- ks_shell.c
+++ ks_shell.c
@@ -9,27 +9,30 @@ int main(void)
 {
 	char *inputptr, *copy_inputptr, *token;
 	char **argv;
-	const char *delim = " ";
-	int num_tok = 0, i;
+	const char *delim = " \0";
+	int num_tok = 0, i, counter;
 	bool receive_cmd = true;
 	size_t x = 0;
 	ssize_t num_charrd;
-
+	
 	while (receive_cmd)
 	{
 		print_promt();
 		num_charrd = getline(&inputptr, &x, stdin);
 		if (num_charrd == -1)
 		{
-			_printf("Closing shell...\n");
+			printf("Exiting shell...\n");
 			exit(EXIT_FAILURE);
 		}
+
+		/* Test if input was received, stored and pointed to*/
+		_printf("%s\n", inputptr);
 
 		/* copy input before using strtok*/
 		copy_inputptr = malloc(sizeof(char) * num_charrd);
 		if (!copy_inputptr)
 			return (-1);
-		_strcpy(copy_inputptr, inputptr);
+		_strcpy(inputptr, copy_inputptr);
 
 		/* use strtok function*/
 		/* make this a function of its own*/
@@ -40,27 +43,27 @@ int main(void)
 			token = strtok(NULL, delim);
 		}
 		num_tok++;
-
+		
 		/* alocate a memory that point to token pointers*/
 		/* make a stand alone funtion*/
 		argv = malloc(sizeof(char *) * num_tok);
 		token = strtok(copy_inputptr, delim);
-		if (argv != NULL)
+		for (i =0; token != NULL; i++)
 		{
-			for (i = 0; token != NULL; i++)
-			{
-				argv[i] = malloc(sizeof(char) * _strlen(token)+1);
-				if (argv[i] == NULL)
-					exit(EXIT_FAILURE);
-			}
+			argv[i] = malloc(sizeof(char) * _strlen(token));
 			_strcpy(argv[i], token);
 			token = strtok(NULL, delim);
 		}
 		argv[i] = NULL;
-		_execmd(argv);
+		
+		/* Test code thus far by printing each stored token*/
+		for (counter = 0; counter < num_tok - 1; counter++)
+		{
+			_printf("%s\n", argv[counter]);
+		}
 
-		/* Free allocated memories*/
-		free_dylloc(argv, num_tok);
+		free(argv);
+		free(inputptr);
 		free(copy_inputptr);
 	}
 	return (0);
